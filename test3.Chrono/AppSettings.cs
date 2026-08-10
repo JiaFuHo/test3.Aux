56929b5e5635b12d337e04ea5809abc321ff7ac1
JiaFuHo <james890124@gmail.com>
2026-08-10 10:19:49 +0800

    v0.1.0

---
+++ test3.Chrono/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace test3.Chrono
+{
+    /// <summary> App設定 </summary>
+    internal class AppSettings
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ test3.Test/Program.cs
@@ -0,0 +1,41 @@
+﻿global using Serilog;
+
+namespace test3.Test
+{
+    /// <summary> 
+    /// App流程: App初始化 →
+    /// 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                if (AppRunner._loggerO != null) { AppRunner._loggerO!.Fatal(ex, "StatusCode = 5200, Message = App Error, ex = "); }
+                else
+                {
+                    Log.Logger = new LoggerConfiguration()
+                                    .WriteTo.Console(
+                                         outputTemplate: "{Timestamp:HH:mm} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                                     )
+                                    .WriteTo.File(
+                                         outputTemplate: "{Timestamp:HH:mm} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                                         path: "C:\\JiaFuHo - GF66\\Programs\\Others\\test3\\test3.Log\\test3.Aux\\test3.Test\\Log_.txt",
+                                         retainedFileCountLimit: null,
+                                         rollingInterval: RollingInterval.Day,
+                                         shared: true
+                                     )
+                                    .MinimumLevel.Verbose()
+                                    .CreateLogger();
+
+                    Log.Fatal(ex, "StatusCode = 5200, Message = App Error, ex = ");
+                }
+            }
+        }
+    }
+}
